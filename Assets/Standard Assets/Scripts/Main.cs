7c1ff6affc6af3e600e7b88dfe974172df53c8fb
ncnuech <ncnuech@umich.edu>
2015-11-02 14:04:46 -0500

    tried to replace incontrol because of matt, but it didnt change things. clear screen at end now

--- Assets/Standard Assets/Scripts/Main.cs
+++ Assets/Standard Assets/Scripts/Main.cs
@@ -94,9 +94,19 @@ public class Main : MonoBehaviour
     public void endGame(bool isTop)
     {
         if (isTop)
+        {
             carTopDone = true;
+            CarmonyGUI.S.topMinimap.SetActive(false);
+            CarmonyGUI.S.topImageLeft.SetActive(false);
+            CarmonyGUI.S.topImageRight.SetActive(false);
+        }
         else
+        {
+            CarmonyGUI.S.bottomMinimap.SetActive(false);
+            CarmonyGUI.S.bottomImageLeft.SetActive(false);
+            CarmonyGUI.S.bottomImageRight.SetActive(false);
             carBottomDone = true;
+        }
         CarmonyGUI.S.endGame(isTop);
     }
 }
