b9b3c10f2d9e105ce4be6d29d69540269f1a6c45
ncnuech <ncnuech@umich.edu>
2015-11-03 16:57:18 -0500

    restructured project. now input from same controllers does not advance letter more than 1

---
+++ Assets/Scripts/CarmonyGUI.cs
@@ -0,0 +1,324 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityStandardAssets.Vehicles.Car;
+
+using InControl;
+
+public class CarmonyGUI : MonoBehaviour {
+
+    static public CarmonyGUI S;
+    public GameObject topGUI;
+    public GameObject bottomGUI;
+    public GameObject progressBar;
+    public GameObject timeText;
+    public GameObject topLetters;
+	public powerUpType topType;
+    public List<GameObject> topLetterList;
+    public GameObject bottomLetters;
+    public List<GameObject> bottomLetterList;
+	public powerUpType bottomType;
+
+    public GameObject topEnd;
+    public GameObject bottomEnd;
+    public GameObject topEndPlace;
+    public GameObject topEndTime;
+    public GameObject bottomEndPlace;
+    public GameObject bottomEndTime;
+
+    public GameObject restartText;
+
+    public GameObject topMinimapDots;
+    public GameObject bottomMinimapDots;
+
+    public GameObject topSwapText;
+    public GameObject bottomSwapText;
+
+    public GameObject topImageLeft;
+    public GameObject topImageRight;
+    public GameObject bottomImageLeft;
+    public GameObject bottomImageRight;
+
+    public Sprite abutton;
+    public Sprite bbutton;
+    public Sprite xbutton;
+    public Sprite ybutton;
+
+	public GameObject topMinimap;
+	public GameObject bottomMinimap;
+
+    bool inLettersTop;
+    bool inLettersBottom;
+    int curIndexBottom = 0;
+    int curIndexTop = 0;
+    List<string> letterListTop;
+    List<string> letterListBottom;
+
+    void Awake()
+    {
+
+        S = this;
+    }
+
+	// Use this for initialization
+	void Start () {
+        topMinimap.SetActive(false);
+        bottomMinimap.SetActive(false);
+		topMinimapDots.SetActive(false);
+		bottomMinimapDots.SetActive(false);
+
+		topType = powerUpType.empty;
+		bottomType = powerUpType.empty;
+
+        bottomImageRight.SetActive(false);
+        bottomImageLeft.SetActive(false);
+        topImageRight.SetActive(false);
+        topImageLeft.SetActive(false);
+
+        foreach (GameObject go in bottomLetterList)
+            go.SetActive(false);
+        foreach (GameObject go in topLetterList)
+            go.SetActive(false);
+    }
+
+    //makes coroutine callable by other classes
+    public void raiseStartFlagText()
+    {
+        StartCoroutine("startFlagText");
+    }
+
+
+    //Co-routine to display GO when race starts then take display down
+    //This utilizes swap text. if need to stylize, must create new text
+    IEnumerator startFlagText()
+    {
+        print("here");
+        topSwapText.SetActive(true);
+        bottomSwapText.SetActive(true);
+        topSwapText.GetComponent<Text>().text = "GO!";
+        bottomSwapText.GetComponent<Text>().text = "GO!";
+
+        yield return new WaitForSeconds(1f);
+        topSwapText.GetComponent<Text>().text = "SWAP";
+        bottomSwapText.GetComponent<Text>().text = "SWAP";
+        topSwapText.SetActive(false);
+        bottomSwapText.SetActive(false);
+    }
+
+	// Return the current index of which letter in the powerup sequence the player is at
+    int getCurIndex(bool isTop)
+    {
+        if (isTop)
+            return curIndexTop;
+        else
+            return curIndexBottom;
+    }
+
+	// Returns whether a button was pressed corresponding to the sequence
+	// TODO Is there a reason this doesn't return a bool. It seems like the hit variable in update is used like a bool
+    bool getHit(bool inLettersTop)
+    {
+		// Get device objects for the correct team
+		CarUserControl userContorl = inLettersTop ? Main.S.carTop.GetComponent<CarUserControl> () : Main.S.carBottom.GetComponent<CarUserControl> ();
+		var playerAInput = InputManager.Devices[userContorl.first];
+        var playerBInput = InputManager.Devices[userContorl.second];
+
+		// Get which letter to check for
+        string letter = "";
+        if (inLettersTop)
+            letter = letterListTop[curIndexTop];
+        else
+            letter = letterListBottom[curIndexBottom];
+
+		// Check for player input of the correct letter
+        bool hit = false;
+        int curIndex = getCurIndex(inLettersTop);
+        
+        switch (letter)
+        {
+            case "A":
+                if (curIndex % 2 == 0)
+                    hit = playerAInput.Action1.WasPressed;
+                else
+                    hit = playerBInput.Action1.WasPressed;
+                break;
+            case "B":
+                if (curIndex % 2 == 0)
+                    hit = playerAInput.Action2.WasPressed;
+                else
+                    hit = playerBInput.Action2.WasPressed;
+                break;
+            case "X":
+                if (curIndex % 2 == 0)
+                    hit = playerAInput.Action3.WasPressed;
+                else
+                    hit = playerBInput.Action3.WasPressed;
+                break;
+            case "Y":
+                if (curIndex % 2 == 0)
+                    hit = playerAInput.Action4.WasPressed;
+                else
+                    hit = playerBInput.Action4.WasPressed;
+                break;
+        }
+        return hit;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        // print("printing: " + inLettersBottom + " " + inLettersTop);
+	    if (inLettersTop)
+        {
+
+            bool hit = getHit(true);
+            if (hit != false)
+            {
+				// Show the player they correctly entered a part of the sequence
+                topLetterList[curIndexTop].GetComponent<Image>().color = new Color32(60, 60, 60, 255);
+                curIndexTop++;
+				// If they finished the sequence clean up the GUI and do the powerup.
+                if (curIndexTop >= letterListTop.Count)
+                {
+                    curIndexTop = 0;
+                    inLettersTop = false;
+					// Blank out the power up sequnce on the screen
+                    for (int i = 0; i < topLetterList.Count; i++)
+                    {
+                        topLetterList[i].SetActive(false);
+                        topLetterList[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                    }
+
+					PowerUp.ActivatePowerUp(true, topType);
+					topType = powerUpType.empty;
+                }
+            }
+        }
+        if (inLettersBottom)
+        {
+            bool hit = getHit(false);
+            if (hit != false)
+            {
+				// Show the player they correctly entered a part of the sequence
+                bottomLetterList[curIndexBottom].GetComponent<Image>().color = new Color32(60, 60, 60, 255);
+                curIndexBottom++;
+				// If they finished the sequence clean up the GUI and do the powerup.
+                if (curIndexBottom >= letterListBottom.Count)
+                {
+                    curIndexBottom = 0;
+                    inLettersBottom = false;
+					// Blank out the power up sequnce on the screen
+                    for(int i = 0; i < bottomLetterList.Count; i++)
+                    {
+                        bottomLetterList[i].SetActive(false);
+                        bottomLetterList[i].GetComponent<Image>().color = new Color(255, 255, 255, 255);
+                    }
+
+					PowerUp.ActivatePowerUp(false, bottomType);
+					bottomType = powerUpType.empty;
+                }
+            } 
+        }
+	}
+
+    public void hideGUI()
+    {
+        topGUI.SetActive(false);
+        bottomGUI.SetActive(false);
+        //timeText.SetActive(false);
+        topLetters.SetActive(false);
+        topMinimap.SetActive(false);
+        bottomMinimap.SetActive(false);
+		topMinimapDots.SetActive(false);
+		bottomMinimapDots.SetActive(false);
+        bottomImageLeft.SetActive(false);
+        bottomImageRight.SetActive(false);
+        topImageLeft.SetActive(false);
+        topImageRight.SetActive(false);
+    }
+
+    public void showGUI()
+    {
+        topGUI.SetActive(true);
+        bottomGUI.SetActive(true);
+        //timeText.SetActive(true);
+        topLetters.SetActive(true);
+        topMinimap.SetActive(true);
+		topMinimapDots.SetActive(true);
+		bottomMinimapDots.SetActive(true);
+        bottomMinimap.SetActive(true);
+        bottomImageLeft.SetActive(true);
+        bottomImageRight.SetActive(true);
+        topImageLeft.SetActive(true);
+        topImageRight.SetActive(true);
+    }
+
+
+    //return the image of the xbox button corresponding to the letter
+    Sprite getSpriteForLetter(string letter)
+    {
+        if (letter == "A")
+            return abutton;
+        else if (letter == "B")
+            return bbutton;
+        else if (letter == "X")
+            return xbutton;
+        else
+            return ybutton;
+    }
+
+	// Display power up sequence to the players 
+    public void setLetters(bool isTopScreen, List<string> letters, powerUpType type)
+    {
+        if (!isTopScreen)
+        {
+            letterListTop = letters;
+            for (int i = 0; i < topLetterList.Count; i++)
+            {
+                topLetterList[i].SetActive(true);
+                topLetterList[i].GetComponent<Image>().sprite = getSpriteForLetter(letters[i]);
+            }
+            inLettersTop = true;
+			topType = type;
+        }
+        else
+        {
+            letterListBottom = letters;
+            for (int i = 0; i < bottomLetterList.Count; i++)
+            {
+                bottomLetterList[i].SetActive(true);
+                bottomLetterList[i].GetComponent<Image>().sprite = getSpriteForLetter(letters[i]);
+            }
+            inLettersBottom = true;
+			bottomType = type;
+        }
+    }
+
+	// Display all end game scenarios
+    public void endGame(bool isTop)
+    {
+        if (isTop)
+        {
+            topEnd.SetActive(true);
+            topEndTime.GetComponent<Text>().text = Main.S.getGameTime();
+            if (!Main.S.carBottomDone)
+                topEndPlace.GetComponent<Text>().text = "1st";
+            else
+                topEndPlace.GetComponent<Text>().text = "2nd";
+        }
+        else
+        {
+            bottomEnd.SetActive(true);
+            bottomEndTime.GetComponent<Text>().text = Main.S.getGameTime();
+            if (!Main.S.carTopDone)
+                bottomEndPlace.GetComponent<Text>().text = "1st";
+            else
+                bottomEndPlace.GetComponent<Text>().text = "2nd";
+        }
+
+        if (Main.S.carBottomDone && Main.S.carTopDone)
+        {
+            restartText.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Vehicles/Car/Scripts/CarUserControl.cs
@@ -0,0 +1,161 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+using InControl;
+using System.Collections;
+//using XInputDotNetPure;
+
+namespace UnityStandardAssets.Vehicles.Car
+{
+    [RequireComponent(typeof (CarController))]
+    public class CarUserControl : MonoBehaviour
+    {
+        private CarController m_Car; // the car controller we want to use
+        public bool isBottomCar;
+
+        public int first = 3; // first - player who turns right and accelerates
+        public int second = 3; // second - player who turns left and brakes
+       
+
+        void Start()
+        {
+        }
+
+		// Show SwapText and start Co-Routine
+        public void playerSwap()
+        {
+            print("in player swap");
+            // Start pulse vibration
+            var playerAInput = InputManager.Devices[first];
+            var playerBInput = InputManager.Devices[second];
+            playerAInput.Vibrate(1f, 1f);
+            playerBInput.Vibrate(1f, 1f);
+            //GamePad.SetVibration((PlayerIndex)first, 1f, 1f);
+            //GamePad.SetVibration((PlayerIndex)second, 1f, 1f);
+            if (!isBottomCar)
+            {
+                CarmonyGUI.S.topSwapText.SetActive(true);
+            }
+            else
+            {
+                CarmonyGUI.S.bottomSwapText.SetActive(true);
+
+            }
+            //call co-routine
+            StartCoroutine("pulseWait");
+
+        }
+
+        IEnumerator pulseWait()
+        {
+            print("in pulse wait");
+
+            yield return new WaitForSeconds(1);
+
+            swapControls();
+        }
+
+		// Actually Swap the players controls
+        void swapControls()
+        {
+            print("in swap controls");
+            // Turn off vibrate
+            //var playerAInput = InputManager.Devices[first];
+            //var playerBInput = InputManager.Devices[second];
+           // playerAInput.Vibrate(0f, 0f);
+            //playerBInput.Vibrate(0f, 0f);
+            //GamePad.SetVibration((PlayerIndex)first, 0f, 0f);
+            //GamePad.SetVibration((PlayerIndex)second, 0f, 0f);
+
+
+			// Remove SwapText and swap control images
+            if (!isBottomCar)
+            {
+                CarmonyGUI.S.topSwapText.SetActive(false);
+                Sprite tempImage = CarmonyGUI.S.topImageLeft.GetComponent<Image>().sprite;
+                CarmonyGUI.S.topImageLeft.GetComponent<Image>().sprite = CarmonyGUI.S.topImageRight.GetComponent<Image>().sprite;
+                CarmonyGUI.S.topImageRight.GetComponent<Image>().sprite = tempImage;
+
+            }
+            else
+            {
+                CarmonyGUI.S.bottomSwapText.SetActive(false);
+                Sprite tempImage = CarmonyGUI.S.bottomImageLeft.GetComponent<Image>().sprite;
+                CarmonyGUI.S.bottomImageLeft.GetComponent<Image>().sprite = CarmonyGUI.S.bottomImageRight.GetComponent<Image>().sprite;
+                CarmonyGUI.S.bottomImageRight.GetComponent<Image>().sprite = tempImage;
+            }
+
+            //swap controls
+            int temp = first;
+            first = second;
+            second = temp;
+         
+        }
+
+        private void Awake()
+        {
+            // get the car controller
+            m_Car = GetComponent<CarController>();
+        }
+
+        //Modify these values in order to tweak steering
+        private void FixedUpdate()
+        {
+
+            if (Time.realtimeSinceStartup < 1)
+                return;
+            if (first >= InputManager.Devices.Count)
+                return;
+            // Use InControl
+            // Hard code the mapping of device to player for now
+            var playerAInput = InputManager.Devices[first];
+            var playerBInput = InputManager.Devices[second];
+
+            // Player A controls accelerator and turning right
+            float accel = playerAInput.RightTrigger;
+            // Player B controls footbrake, handbrake, and turning left
+            float footbrake = -1f * playerBInput.LeftTrigger;
+            //Turning off handbrake because we are already using that button
+            // for the powerup character sequence input.
+            float handbrake = 0;
+
+            float playerA_turnRight = Math.Max(0f, playerAInput.LeftStickX);
+            float playerB_turnLeft = Math.Min(0f, playerBInput.LeftStickX);
+
+            float steering = playerA_turnRight + playerB_turnLeft;
+			if (gameObject.transform.localScale.x < 1) {
+				steering *= Mathf.Pow(2.718f, gameObject.GetComponent<CarController> ().getSpeed()/-60); 
+				gameObject.GetComponent<CarController> ().SlipLimit = .7f;
+				gameObject.GetComponent<CarController> ().SteerHelperProperty = 1f;
+			} else {
+				gameObject.GetComponent<CarController> ().SlipLimit = .3f;
+				gameObject.GetComponent<CarController> ().SteerHelperProperty = .644f;
+			}
+
+            // pass the input to the car!
+            if (!isBottomCar)
+            {
+                if (Main.S.carTopDone)
+                {
+                    m_Car.Move(0f, 0f, 0f, 0f);
+                    return;
+                }
+            }
+            else
+            {
+                if (Main.S.carBottomDone)
+                {
+                    m_Car.Move(0f, 0f, 0f, 0f);
+                    return;
+                }
+            }
+#if !MOBILE_INPUT
+            //float handbrake = CrossPlatformInputManager.GetAxis("Jump");
+            m_Car.Move(steering, accel, footbrake, handbrake);
+#else
+            m_Car.Move(steering, accel, footbrake, 0f);
+#endif
+        }
+    }
+}
